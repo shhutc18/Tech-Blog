10ae386d1475fe706b74a55871f6c79228f0b98c
Shelby Hutchinson <shhutc18@gmail.com>
2024-02-11 19:37:51 -0800

    fixing error msg

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,23 @@
-// Import the Sequelize constructor from the `sequelize` package
+// Import the Sequelize constructor from the sequelize package
 const Sequelize = require('sequelize');
 
-// Import and configure dotenv to load environment variables
+// Import dotenv package to read and set any environment variables
 require('dotenv').config();
 
-// Declare a variable to hold the Sequelize instance
+// Declare a variable to hold our Sequelize instance
 let sequelize;
 
-// Check if the application is running on JawsDB on Heroku
+// If the application is running on Heroku and it has a JAWSDB_URL environment variable (which is automatically set by the JAWSDB add-on), use that as the connection URL
 if (process.env.JAWSDB_URL) {
-  // If so, initialize Sequelize to connect to the database using the Heroku database URL
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // If the app is running locally, initialize Sequelize to connect to a local MySQL database
-  sequelize = new Sequelize(
-    process.env.DB_NAME, // The name of the database
-    process.env.DB_USER, // The user to connect as
-    process.env.DB_PASSWORD, // The password for the user
-    {
-      host: 'localhost', // The host of the database
-      dialect: 'mysql', // Connect to the database using the MySQL dialect
-      port: 3001 // The port on which the database is running
-    }
-  );
+  // Otherwise, connect to the MySQL database specified by the DB_NAME, DB_USER, and DB_PW environment variables
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost', // The host of the database. This is typically 'localhost' when the database is running on the same machine as this application
+    dialect: 'mysql', // The type of SQL database. In this case, it's MySQL
+    port: 3306 // The port number on which the database server is listening
+  });
 }
 
-// Export the Sequelize connection instance to be used in other parts of the application
+// Export the sequelize object so it can be used in other parts of the application
 module.exports = sequelize;
