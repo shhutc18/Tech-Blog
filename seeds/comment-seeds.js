96c4e8c4a8b11926fdc73848393e5fc564de945a
Shelby Hutchinson <shhutc18@gmail.com>
2024-02-11 21:16:59 -0800

    correcting user and post id's

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -5,14 +5,14 @@ const { Comment } = require('../models');
 const commentData = [
   {
     // The first comment is made by the user with ID 6 on the post with ID 3 and says "This is a great resource, thanks for sharing!"
-    user_id: 6,
-    post_id: 3,
+    user_id: 1,
+    post_id: 2,
     comment_text: "This is a great resource, thanks for sharing!"
   },
   {
     // The second comment is made by the user with ID 7 on the post with ID 2 and says "I found this very helpful, thank you!"
-    user_id: 7,
-    post_id: 2,
+    user_id: 2,
+    post_id: 1,
     comment_text: "I found this very helpful, thank you!"
   },
   // More comments can be added as needed
